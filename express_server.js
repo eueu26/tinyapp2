add14487dd1bb73e5eb6b65891f72bff8aff31b3
Eunice <117746227+Eueu26@users.noreply.github.com>
2023-05-06 17:12:41 -0400

    corrected urlDatabase variable format

--- express_server.js
+++ express_server.js
@@ -3,7 +3,7 @@ const app = express();
 const PORT = 8080;
 
 const generateRandomString = function() {
-  return Math.random().toString(36).substring(2, 6);
+  return Math.random().toString(36).substring(2, 8);
 };
 
 app.set("view engine", "ejs");
@@ -11,14 +11,22 @@ app.set("view engine", "ejs");
 
 app.use(express.urlencoded({ extended: true }));
 
-let urlDatabase = [
-  {id: "b2xVn2", longUrl: "http://www.lighthouselabs.ca"},
-  {id: "9sm5xK", longUrl: "http://www.google.com"}
-];
+const urlDatabase = {
+  "b2xVn2": "http://www.lighthouselabs.ca",
+  "9sm5xK": "http://www.google.com"
+};
+
+
+// app.get("/u/:id", (req, res) => {
+//   const longURL = "http://www.lighthouselabs.ca";
+//   res.redirect(longURL);
+// });
 
 app.post("/urls", (req, res) => {
   console.log(req.body);
   res.send("Ok");
+  // let id = generateRandomString();
+  // res.redirect(`/urls/${id}`);
 });
 
 app.get("/urls/new", (req, res) => {
@@ -26,12 +34,17 @@ app.get("/urls/new", (req, res) => {
 });
 
 app.get("/urls/:id", (req, res) => {
-  let templateVars = {id: req.params.id, longUrl: "http://www.lighthouselabs.ca"};
+  let templateVars = {id: req.params.id, longURL: "http://www.lighthouselabs.ca"};
   res.render("urls_show", templateVars);
 });
 
 app.get("/urls", (req, res) => {
-  res.render("urls_index.ejs", {urlDatabase: urlDatabase});
+  const templateVars = {urls: urlDatabase };
+  res.render("urls_index.ejs", templateVars);
+});
+
+app.get("/hello", (req, res) => {
+  res.send("<html><body>Hello <b>World</b></body></html>\n");
 });
 
 app.get("/urls.json", (req, res) => {
@@ -42,13 +55,6 @@ app.get("/", (req, res) => {
   res.send("Hello!");
 });
 
-app.get("/hello", (req, res) => {
-  res.send("<html><body>Hello <b>World</b></body></html>\n");
-});
-
-
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
 });
-
-generateRandomString();
