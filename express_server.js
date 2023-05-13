9df75674f825b5f9764953f38c6f66264e366b7f
Eunice <117746227+Eueu26@users.noreply.github.com>
2023-05-13 16:38:12 -0400

    add login form and display username

--- express_server.js
+++ express_server.js
@@ -1,20 +1,34 @@
 const express = require("express");
 const app = express();
 const PORT = 8080;
+const cookieParser = require('cookie-parser');
 
 const generateRandomString = function() {
   return Math.random().toString(36).substring(2, 8);
 };
 
-app.set("view engine", "ejs");
-
-
 app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
+
+app.set("view engine", "ejs");
 
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
+
+// app.get("/login", (req, res) => {
+//   const templateVars = { Username: users[req]
+//   }
+// })
+
+app.post("/login", (req, res) => {
+  const username = req.body.username;
+
+  res.cookie("username", username);
+  res.redirect(`/urls`);
+});
+
 
 app.post("urls/:id/delete", (req, res) => {
   const id = req.params.id;
@@ -50,8 +64,8 @@ app.get("/urls/:id", (req, res) => {
 });
 
 app.get("/urls", (req, res) => {
-  const templateVars = {urls: urlDatabase };
-  res.render("urls_index.ejs", templateVars);
+  const templateVars = {username: req.cookies["username"], urls: urlDatabase };
+  res.render("urls_index", templateVars);
 });
 
 app.get("/hello", (req, res) => {
